36f0ad2b962d235eba8ae4d30fe4728197fe1c89
Sipho <sisusasipho@gmail.com>
2026-04-08 18:05:39 +0200

    CORS commentary update

--- app.py
+++ app.py
@@ -121,7 +121,7 @@ CURRENCY_DRIVERS = {
 @app.after_request
 def add_cors_headers(response):
     response.headers["Access-Control-Allow-Origin"] = "*"
-    response.headers["Access-Control-Allow-Methods"] = "GET,OPTIONS"
+    response.headers["Access-Control-Allow-Methods"] = "GET,POST,OPTIONS"
     response.headers["Access-Control-Allow-Headers"] = "Content-Type"
     return response
 
